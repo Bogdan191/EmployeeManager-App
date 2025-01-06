c64c2c51ce2fa428f77ec961930de3ce84280a67
Bogdan191 <bogdanpintilie00@yahoo.com>
2025-01-06 16:47:24 +0200

    Initial commit: add the back-end support of the app

---
+++ employeemanager/src/main/java/com/dev/bogdan/employeemanager/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package com.dev.bogdan.employeemanager.service;
+
+import com.dev.bogdan.employeemanager.exception.UserNotFoundException;
+import com.dev.bogdan.employeemanager.model.Employee;
+import com.dev.bogdan.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee editEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User by id" + id + "was not found"));
+    }
+
+    public void deleteEmployeeById(Long id) {
+        employeeRepo.delete(findEmployeeById(id));
+    }
+}
