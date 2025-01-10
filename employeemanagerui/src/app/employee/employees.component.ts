f3997abc50bb22fd731937d1b09d55e48432d1c7
Bogdan191 <bogdanpintilie00@yahoo.com>
2025-01-10 19:11:50 +0200

    Add search action

--- employeemanagerui/src/app/employee/employees.component.ts
+++ employeemanagerui/src/app/employee/employees.component.ts
@@ -81,6 +81,21 @@ export class EmployeesComponent implements OnInit {
         );
     }
 
+    public searchEmployees(key: string) : void {
+        console.log(key);
+        const results: Employee[] = [];
+        for(const employee of this.employees) {
+            if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+              results.push(employee);
+            }
+        }
+
+        this.employees = results; 
+        if(results.length === 0 || !key) {
+            this.getEmployees(); 
+        }
+    }
+
 
     
   public onOpenModal(employee: Employee | null, mode: string): void {
